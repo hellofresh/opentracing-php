7fa228c8b0b7ef5c78174688a713e49f57b2ba46
Warnar Boekkooi <wbo@hellofresh.com>
2017-04-13 12:22:02 +0200

    Ensure baggage item values are strings

--- tests/BasicTracer/SpanContextTest.php
+++ tests/BasicTracer/SpanContextTest.php
@@ -22,7 +22,22 @@ class SpanContextTest extends TestCase
         $this->assertSame('test', $context->getTraceId());
         $this->assertSame(1, $context->getSpanId());
         $this->assertTrue($context->isSampled());
-        $this->assertEquals($baggage, $context->getBaggageItems());
+        $this->assertSame($baggage, $context->getBaggageItems());
+    }
+
+    /**
+     * @test
+     */
+    public function itWillCastBaggageValuesToString()
+    {
+        $expectedBaggage = ['this' => '1', 'baggage' => '123'];
+
+        $context = new SpanContext('test', 1, true, ['this' => true, 'baggage' => 123]);
+
+        $this->assertSame(
+            $expectedBaggage,
+            $context->getBaggageItems()
+        );
     }
 
     /**
@@ -30,20 +45,20 @@ class SpanContextTest extends TestCase
      */
     public function baggageChangesShouldNotBeReflectedInTheContext()
     {
-        $baggage = ['test' => 0];
+        $baggage = ['test' => '0'];
 
         $context = new SpanContext('test', 1, true, $baggage);
         $this->assertCount(1, $context->getBaggageItems());
-        $this->assertEquals(0, $context->getBaggageItems()['test']);
+        $this->assertSame('0', $context->getBaggageItems()['test']);
 
         $baggage['test'] = 1;
         $this->assertCount(1, $context->getBaggageItems());
-        $this->assertEquals(0, $context->getBaggageItems()['test']);
+        $this->assertSame('0', $context->getBaggageItems()['test']);
 
         $baggage = $context->getBaggageItems();
         $baggage['test'] = 2;
         $baggage['testing'] = true;
         $this->assertCount(1, $context->getBaggageItems());
-        $this->assertEquals(0, $context->getBaggageItems()['test']);
+        $this->assertSame('0', $context->getBaggageItems()['test']);
     }
 }
